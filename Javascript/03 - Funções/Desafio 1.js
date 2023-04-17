1baeae0a4fde46e774d3bf64ae171afea0c5d924
RafaCartaxo <rafael.cartaxo@hotmail.com>
2023-04-17 20:50:33 -0300

    Iniciando orientação a objetos com JavaScript

--- Javascript/03 - Funções/Desafio 1.js
+++ Javascript/03 - Funções/Desafio 1.js
@@ -1,10 +1,7 @@
-
 /*
 function escrevaNome(nome) {
    return 'Meu nome é ' + nome;
 }
-
-
 function verificarIdade(idade) {
     if (idade < 18) {
         console.log(escrevaNome('Rafael ') + 'e sou menor de idade');
@@ -13,32 +10,31 @@ function verificarIdade(idade) {
         console.log('Maior de idade');
     }
 }
-
 verificarIdade(17);
 */
 
 
 function aplicarDesconto(valor, desconto) {
- return valor - (valor * (desconto / 100));
-}
-
-function aplicarJuros(valor, juros) {
-    return valor + (valor * (juros / 100));
-    
-}
-
-const precoEtiqueta = 100;
-const formaDePagamento = 4;
-
-if (formaDePagamento === 1) {
-    console.log(aplicarDesconto(precoEtiqueta, 10));
-
-}   else if (formaDePagamento === 2) {
-    console.log(aplicarDesconto(precoEtiqueta, 15));
-
-} else if (formaDePagamento === 3) {
-    console.log(precoEtiqueta);
-
-} else {
-    console.log(aplicarJuros(precoEtiqueta, 10));
-}
+    return valor - (valor * (desconto / 100));
+   }
+   
+   function aplicarJuros(valor, juros) {
+       return valor + (valor * (juros / 100));
+       
+   }
+   
+   const precoEtiqueta = 100;
+   const formaDePagamento = 4;
+   
+   if (formaDePagamento === 1) {
+       console.log(aplicarDesconto(precoEtiqueta, 10));
+   
+   }   else if (formaDePagamento === 2) {
+       console.log(aplicarDesconto(precoEtiqueta, 15));
+   
+   } else if (formaDePagamento === 3) {
+       console.log(precoEtiqueta);
+   
+   } else {
+       console.log(aplicarJuros(precoEtiqueta, 10));
+   }
