398fa811dcc6c6dce39dafd673de63f0bff040f3
RafaCartaxo <rafael.cartaxo@hotmail.com>
2023-04-18 18:01:52 -0300

    Classificar IMC de acordo com peso

--- Javascript/04 - Objetos/desafio2.js
+++ Javascript/04 - Objetos/desafio2.js
@@ -20,10 +20,35 @@ class Pessoa{
     calcularImc(peso, altura){
         return this.peso / (this.altura * this.altura);
     }
+
+    classificarImc(){
+    const imc = this.calcularImc();
+   
+    if (imc < 18.5) {
+        return ('Abaixo do peso.');
+    
+    } else if (imc >= 18.5 && imc <= 25) {
+        return ('Peso normal.')
+        
+    } else if (imc >= 25 && imc <= 30){
+        return ('Acima do peso.');
+    
+    } else if (imc >= 30 && imc <= 40){
+        return ('Obeso.');
+    
+    } else {
+        return ('Obesidade grave');
+    }
+
+    }
 }
 
 const jose = new Pessoa ('José', 70, 1.75);
-console.log('Meu nome é José e meu IMC é: ' + jose.calcularImc(70, 1.75));
+console.log('Meu nome é José e meu IMC é: ' + jose.classificarImc(70, 1.75));
 
 const rafael = new Pessoa('Rafael', 83, 1.85);
-console.log('Meu nome é Rafael e meu IMC é: ' + rafael.calcularImc(83, 1.85));
+console.log('Meu nome é Rafael e meu IMC é: ' + rafael.classificarImc(83, 1.85));
+
+const gabrielle = new Pessoa('Gabrielle', 50, 1.69);
+console.log('Meu nome é Gabrielle e meu IMC é: ' + gabrielle.classificarImc(57, 1.69));
+
