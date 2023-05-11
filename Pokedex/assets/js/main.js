aa6cf036b9b809e2e1af183d71528bfb8837531d
RafaCartaxo <rafael.cartaxo@hotmail.com>
2023-05-11 19:07:12 -0300

    Convertendo API para modelo personalizado

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,29 +1,24 @@
-function convertPokemonTypesLi(pokemonTypes){
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
 
-}
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-                    <span class="number">#${pokemon.order}</span>
+        <li class="pokemon${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
                     <span class="name">${pokemon.name}</span>
 
                     <div class="detail"> 
                         <ol class="types">
-                           ${convertPokemonTypesLi(pokemon.types).join('')}
+                           ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                         </ol>
-                        <img src="${pokemon.sprites.other.dream_world.front_default}" 
-                        alt="${pokemon.name}">
+                        <img src="${pokemon.photo}" 
+                            alt="${pokemon.name}">
                     </div>
-
         </li>
     `
     
 }
 
 const pokemonList = document.getElementById('pokemonList')
-
 
 pokeApi.getPokemons().then((pokemons = []) => {
     const newHtml = pokemons.map(convertPokemonToLi).join('')
