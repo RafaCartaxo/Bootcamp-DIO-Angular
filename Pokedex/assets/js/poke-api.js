a8be187edb7e0b7d9c71981fd551a6a3a6b29059
RafaCartaxo <rafael.cartaxo@hotmail.com>
2023-05-15 16:02:43 -0300

    Tipos de pokemon dinamicamente

--- Pokedex/assets/js/poke-api.js
+++ Pokedex/assets/js/poke-api.js
@@ -1,14 +1,13 @@
-
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const[type] = types
-    
+    const [type] = types
+
     pokemon.types = types
     pokemon.type = type
 
@@ -17,22 +16,19 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     return pokemon
 }
 
-
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-    .then((response) => response.json())
-    .then(convertPokeApiDetailToPokemon)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = function (offset = 0, limit = 5) {
+pokeApi.getPokemons = (offset = 0, limit = 30) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit${limit}`
-
-return fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => jsonBody.results)
-.then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-.then((detailRequests) => Promise.all(detailRequests))
-.then((pokemonDetails) => pokemonDetails)
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
-
