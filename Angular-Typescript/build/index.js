5a8ad0ebdf41eea5932b9bb316ec12f88a04fd18
RafaCartaxo <rafael.cartaxo@hotmail.com>
2023-07-03 17:40:33 -0300

    Classes e subclasses

--- Angular-Typescript/build/index.js
+++ Angular-Typescript/build/index.js
@@ -1,8 +1,87 @@
 "use strict";
-function printaObjeto(pessoa) {
-    console.log(pessoa);
+//Objects e variáveis
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+let produto = {
+    name: "Rafael",
+    cidade: "João Pessoa",
+    idade: 23,
+};
+let meuProduto = {
+    nome: "Tênis",
+    preco: 89.99,
+    unidades: 2,
+};
+//Arrays
+let dados = ["Borges", "Rafael", "Gabrielle"];
+let dados2 = ["Borges", "Rafael", "Gabrielle",];
+//Arrays multi types
+let infos = ["Borges", "Rafael", "Gabrielle", 1, 2, 3, 4, 5];
+//Tuplas
+let boleto = ["agua conta", 199.89, 180811833];
+dados.pop();
+//Datas
+let aniversario = new Date("2022-12-01 05:00");
+console.log(aniversario.toString());
+//Funções
+function addNumber(x, y) {
+    return x + y;
 }
-printaObjeto({
-    name: "Bruce Wayne",
-    vulgo: "Batman",
-});
+function addToHello(name) {
+    return `Hello ${name}`;
+}
+function callToPhone(phone) {
+    return phone;
+}
+let soma = addNumber(4, 7);
+console.log(soma);
+console.log(addToHello("Rafael"));
+console.log(callToPhone("8888-8888"));
+//Funções assíncronas - async
+function getDatabase(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return "Rafael";
+    });
+}
+const bot1 = {
+    id: 1,
+    name: "Megaman",
+};
+const bot2 = {
+    id: 1,
+    name: "Megaman",
+    sayHello: function () {
+        throw new Error("Function not implemented.");
+    }
+};
+class Pessoa {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    sayHello() {
+        return `Hello! I'am ${this.name}.`;
+    }
+}
+const p = new Pessoa(1, "Gustman");
+console.log(p.sayHello());
+//classes
+class Character {
+    constructor(name, strength, skill) {
+        this.name = name;
+        this.strength = strength;
+        this.skill = skill;
+    }
+    attack() {
+        console.log(`Attack with ${this.strength} points`);
+    }
+}
+const p1 = new Character("Ryu", 2, 50);
+p1.attack;
--- Angular-Typescript/src/index.ts
+++ Angular-Typescript/src/index.ts
@@ -1,13 +1,133 @@
-type heroi = {
-    name: string;
-    vulgo: string;
-};
+//Objects e variáveis
 
-function printaObjeto(pessoa: heroi){
-console.log(pessoa);
+let produto: object = {
+    name: "Rafael",
+    cidade: "João Pessoa",
+    idade: 23,
 }
 
-printaObjeto({
-    name: "Bruce Wayne",
-    vulgo: "Batman",
-});
+//Objeto tipado - com previsibilidade
+type ProdutoLoja = {
+    nome: string;
+    preco: number;
+    unidades: number;
+};
+
+let meuProduto: ProdutoLoja = {
+    nome: "Tênis",
+    preco: 89.99,
+    unidades: 2,
+};
+
+
+
+//Arrays
+
+let dados: string [] = ["Borges", "Rafael", "Gabrielle"];
+let dados2: Array<string> = ["Borges", "Rafael", "Gabrielle",];
+
+//Arrays multi types
+
+let infos: (string | number)[] = ["Borges", "Rafael", "Gabrielle", 1, 2, 3, 4, 5];
+
+//Tuplas
+
+let boleto:[string, number, number] = ["agua conta", 199.89, 180811833];
+
+dados.pop();
+
+//Datas
+
+let aniversario:Date = new Date("2022-12-01 05:00")
+console.log(aniversario.toString())
+
+//Funções
+
+function addNumber (x: number, y: number): number {
+return x + y;
+}
+
+function addToHello(name: string){
+    return `Hello ${name}`;
+}
+
+function callToPhone(phone: number | string): number | string {
+    return phone;
+}
+
+let soma: number = addNumber(4, 7);
+
+console.log(soma);
+console.log(addToHello("Rafael"));
+console.log(callToPhone("8888-8888"));
+
+//Funções assíncronas - async
+
+async function getDatabase(id: number): Promise<string>{
+    return "Rafael";
+}
+
+
+//Interfaces
+
+type robot1 = {
+    id: number | string;
+    name: string;
+};
+
+interface robot2{
+    readonly id: number | string;
+    name: string;
+    sayHello(): string;
+}
+
+
+const bot1: robot1 = {
+    id: 1,
+    name: "Megaman",
+}
+
+const bot2: robot2 = {
+    id: 1,
+    name: "Megaman",
+    sayHello: function (): string {
+        throw new Error("Function not implemented.");
+    }
+}
+
+
+class Pessoa implements robot2{
+    id: string | number;
+    name: string;
+
+constructor(id: string | number, name: string){
+    this.id = id;
+    this.name = name;
+}
+    sayHello(): string {
+        return `Hello! I'am ${this.name}.`;
+    }
+}
+
+const p = new Pessoa(1, "Gustman")
+console.log(p.sayHello())
+
+//classes
+class Character {
+    name: string;
+    strength: number;
+    skill: number;
+
+    constructor(name: string, strength: number, skill: number){
+    this.name = name;
+    this.strength = strength;
+    this.skill = skill;
+    }
+
+    attack() : void {
+        console.log(`Attack with ${this.strength} points`);
+    }
+}
+
+const p1 = new Character("Ryu", 2, 50);
+p1.attack;
