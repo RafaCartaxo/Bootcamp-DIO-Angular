1c8b9c087330ce0f1b12b57bd06bb3d42611379c
RafaCartaxo <rafael.cartaxo@hotmail.com>
2023-07-04 17:41:53 -0300

    Decorators

--- Angular-Typescript/build/index.js
+++ Angular-Typescript/build/index.js
@@ -1,5 +1,11 @@
 "use strict";
 //Objects e variáveis
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -99,6 +105,15 @@ function concatArray(...itens) {
     return new Array().concat(...itens);
 }
 const numArray = concatArray([1, 5], [3]);
-const stgArray = concatArray(["Rafael", "Felipe", "João"]);
+const stgArray = concatArray(["Rafael", "Felipeee", "João"]);
 console.log(numArray);
 console.log(stgArray);
+//decorators
+function ExibirNome(target) {
+    console.log(target);
+}
+let Funcionario = class Funcionario {
+};
+Funcionario = __decorate([
+    ExibirNome
+], Funcionario);
--- Angular-Typescript/src/index.ts
+++ Angular-Typescript/src/index.ts
@@ -155,7 +155,66 @@ function concatArray<T>(...itens: T[]): T[]{
 }
 
 const numArray = concatArray<number[]>([1, 5], [3]);
-const stgArray = concatArray<string[]>(["Rafael", "Felipe", "João"]);
+const stgArray = concatArray<string[]>(["Rafael", "Felipeee", "João"]);
 
 console.log(numArray);
 console.log(stgArray);
+
+
+//decorators
+
+function ExibirNome(target: any){
+    console.log(target);
+}
+
+@ExibirNome
+class Funcionario {}
+
+@ExibirNome
+class Quincas {}
+
+
+function apiVersion(version: string){
+    return (target: any) => {
+        Object.assign(target.prototype, { __version: version, __name: "Rafael" });
+    };
+}
+
+@apiVersion("1.10")
+//class Api {}
+
+//const api = new Api();
+//console.log(api);
+
+//attribute decorator
+//attribute decorator
+function minLength(length: number) {
+  return (target: any, key: string) => {
+    let _value = target[key];
+
+    const getter = () => "[play]" + _value;
+    const setter = (value: string) => {
+      if (value.length < length) {
+        throw new Error(`Tamanho menor do que ${length}`);
+      } else {
+        _value = value;
+      }
+    };
+
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+    });
+  };
+}
+
+class Api {
+  @minLength(10)
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const api = new Api("usaada41324654");
